e2e47a810a378fac71cd9c32ca0ff0143b5f6aec
hlee13 <55885293@qq.com>
2021-12-17 11:20:01 +0800

    Update f_gan.py

--- f_gan.py
+++ f_gan.py
@@ -29,26 +29,44 @@ print tf.__version__
 # MAX_GRAD_NORM = 1e-1
 epoch = 1000
 embedding_dim = 3
-lr = 1e-2
-batch_size = 1000
-LAMBDA=5e2
-act = tf.nn.elu
+lr = 1e-3
+batch_size = 2000
+LAMBDA=1e1
+act = tf.nn.selu
 MAX_NORM=1.
 
 # optimizer = tf.train.GradientDescentOptimizer(lr)
 # optimizer = tf.train.AdadeltaOptimizer(lr)
 # optimizer = tf.train.AdagradOptimizer(lr)
 # optimizer = tf.train.AdaMaxOptimizer(lr)
-optimizer = tf.train.AdamOptimizer(lr)
-# optimizer = tf.train.RMSPropOptimizer(lr)
+# optimizer = tf.train.AdamOptimizer(lr)
+optimizer = tf.train.RMSPropOptimizer(lr)
 # optimizer = tf.train.FtrlOptimizer(lr)
 
 x = np.array(range(3))
-prob0 = np.array([0.8, 0.1, 0.1])
-prob1 = np.array([0.3, 0.25, 0.45])
+prob0 = np.array([0.7, 0.1, 0.2])
+prob1 = np.array([0.2, 0.1, 0.7])
+# prob1 = np.array([0.3, 0.25, 0.45])
 
 print scipy.stats.entropy(prob0, prob1)
 print scipy.stats.entropy(prob1, prob0)
+
+# clip model weights to a given hypercube
+
+class ClipConstraint(tf.keras.constraints.Constraint):
+	# set clip value when initialized
+	def __init__(self, clip_value):
+		self.clip_value = clip_value
+ 
+	# clip model weights to hypercube
+	def __call__(self, weights):
+		return tf.keras.backend.clip(weights, -self.clip_value, self.clip_value)
+ 
+	# get the config
+	def get_config(self):
+		return {'clip_value': self.clip_value}
+const = ClipConstraint(0.1)
+
 
 embedding_x = fc.categorical_column_with_vocabulary_list(
     'x', [d for d in range(0, 100)], dtype=tf.int32, num_oov_buckets=1)
@@ -81,20 +99,24 @@ def net(name, _input, layer_units, act=act, out_units=1):
   out = _input
   for ix, units in enumerate(layer_units):
     out = tf.layers.dense(out, units=units, activation=act, name="{0}_L{1}".format(name, ix),
-      kernel_initializer=tf.truncated_normal_initializer(mu, stddev)
+      # kernel_initializer=tf.truncated_normal_initializer(mu, stddev)
       # ,kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-4)
       # ,bias_regularizer=tf.contrib.layers.l2_regularizer(1e-4)
-      ,kernel_constraint=tf.keras.constraints.max_norm(MAX_NORM)
-      ,bias_constraint=tf.keras.constraints.max_norm(MAX_NORM)
+      # kernel_constraint=tf.keras.constraints.max_norm(MAX_NORM)
+      # ,bias_constraint=tf.keras.constraints.max_norm(MAX_NORM)
+      kernel_constraint=const
+      ,bias_constraint=const
       ,reuse=tf.AUTO_REUSE
       )
   
   out = tf.layers.dense(out, units=out_units, name="{0}".format(name),
-      kernel_initializer=tf.truncated_normal_initializer(mu, stddev)
+      # kernel_initializer=tf.truncated_normal_initializer(mu, stddev)
       # ,kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-4)
       # ,bias_regularizer=tf.contrib.layers.l2_regularizer(1e-4)
-      ,kernel_constraint=tf.keras.constraints.max_norm(MAX_NORM)
-      ,bias_constraint=tf.keras.constraints.max_norm(MAX_NORM)
+      kernel_constraint=const
+      ,bias_constraint=const
+      # kernel_constraint=tf.keras.constraints.max_norm(MAX_NORM)
+      # ,bias_constraint=tf.keras.constraints.max_norm(MAX_NORM)
       ,reuse=tf.AUTO_REUSE
       )
   return out
@@ -117,7 +139,10 @@ gradients = tf.gradients(net('NN', _x, [128,128,128,10]), [_x])[0]
 # slopes = tf.sqrt(tf.reduce_mean(tf.square(gradients)/tf.reshape(tf.norm(gradients, axis=1), (-1,1)), reduction_indices=[1]))
 # slopes = tf.sqrt(tf.reduce_mean(tf.square(gradients), reduction_indices=[1]) + 1.0e-12)
 slopes = tf.norm(gradients, axis=1)
+print(gradients.shape)
+# print(slopes.shape)
 gradient_penalty = tf.reduce_mean((slopes-1.) ** 2)
+# gradient_penalty = tf.reduce_mean(tf.maximum(slopes-1., 0.) ** 2)
 
 with tf.name_scope('optimizer'):
   global_step = tf.Variable(0, name="global_step", trainable=False)
@@ -152,7 +177,7 @@ with tf.Session() as sess:
     _, _step, _loss, _data, _logits = sess.run([train_op, global_step, loss, train_data, logits1])
 
     # if _step % 1:
-    if _step % 100 == 0:
+    if _step % 1 == 0:
       print _step, _loss, {ix.tolist()[0]:v.tolist()[0] for ix, v in zip(_data[1], _logits)}
     else:
       print _step, _loss
